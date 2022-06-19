72860b215418a26450f13403928bfdefa4ca9232
Janus022012 <kamei022012@gmail.com>
2022-06-20 06:03:41 +0900

    2022/6/20

---
+++ Book/TeachYouselfC++/code/chap2/2-1-1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+struct S{
+    int x;
+    int y;
+};
+
+union U{
+    S s;
+};
+
+int main(){
+    U u = {};
+    u.s.x = 10;
+    u.s.y = 20;
+
+    std::cout << u.s.x << " " << u.s.y << std::endl;
+
+    return 0;
+}
